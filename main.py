28922bb7f27a619ceb2072e2f50d625fdfcf5548
Hywoju <Youri.Vissers@gmail.com>
2024-05-02 13:13:19 +0200

    Diff imaging improved

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import T03_ObsM37_tools as WT
 from astropy.io import fits
 import matplotlib.colors as colors
 from regions import Regions
+import matplotlib.colors as colors
+import fits_compress
 
 path = "C:\Leiden Universiteit\ALOP\La_Palma"
 
@@ -29,6 +31,7 @@ path = "C:\Leiden Universiteit\ALOP\La_Palma"
 
 """Reads in regions form DS9, normalized data and subtracts two data sets"""
 
+
 def normalize(array):
     return array / np.sum(array)
 
@@ -37,22 +40,35 @@ def diff_image(array_1, array_2):
     return array_1 - array_2
 
 
-data_old = fits.open(path + '\Drew_data\\r376531-4_test2.fits')[0]
-data_new = fits.open(path + '\Drew_data\\r376531-4_test2.fits')[0]
-reg_str_old = '\Drew_data\\r376531-4_test2_2box.reg'
-reg_str_new = '\Drew_data\\r376531-4_test2_2box.reg'
+# data_old = fits.open(path + '\Drew_data\sections\\region_1_2003\\r376535-4.fits')[1]
+# data_old = fits_compress.decompress_fits([data_old])[0]
+data_new = fits.open(path + '\Drew_data\sections\\region_1_2024\\coadd.fits')[0]
+reg_str_IM_old = '\Regions\Section1_2003_1box_IM.reg'
+reg_str_IM_new = '\Regions\Section1_2024_1box_IM.reg'
 
-regs_old = Regions.read(path + reg_str_old, format='ds9')
-regs_new = Regions.read(path + reg_str_new, format='ds9')
+print(data_old.data.shape)
 
-slices = np.zeros(1,dtype=[('old','O'),('new','O')])
+regs_old = Regions.read(path + reg_str_IM_old, format='ds9')
+regs_new = Regions.read(path + reg_str_IM_new, format='ds9')
 
-for i,j,k in zip([data_old, data_new],[reg_str_old,reg_str_new],['old','new']):
-    regs = Regions.read(path + j, format='ds9')
+slices = np.zeros(len(regs_old), dtype=[('old', 'O'), ('new', 'O')])
 
+for dat, regs, k in zip([data_old, data_new], [regs_old, regs_new], ['old', 'new']):
 
-    for i,reg in enumerate(regs):
-        slice = data_old.data[int(reg.center.x - (0.5 * reg.width)):int(reg.center.x + (0.5 * reg.width)),
-                      int(reg.center.y - (0.5 * reg.height)):int(reg.center.y + (0.5 * reg.height))]
+    for i, reg in enumerate(regs):
+        slice = dat.data[int(reg.center.x - (0.5 * reg.width)):int(reg.center.x + (0.5 * reg.width)),
+                int(reg.center.y - (0.5 * reg.height)):int(reg.center.y + (0.5 * reg.height))]
+        slices[k][i] = normalize(slice)
+
+print(slices['old'][0].shape)
+plt.imshow(np.rot90(slices['old'][0]), cmap='hot', origin='lower', norm=colors.LogNorm(1e-1,1e-5))
+plt.colorbar(orientation='horizontal')
+plt.show()
+
+for i in range(len(slices)):
+    diff = diff_image(slices['old'][i]-slices['new'][i])
+    hdu = fits.PrimaryHDU(data=diff)
+    hdu.writeto(path + f'\Diff_images\\section1_diffim_{i}.fits')
+
 
 
